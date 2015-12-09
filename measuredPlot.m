964c933ba0690db924580b2b0f5a43845e9e12e0
rnwatanabe <renato.navillewatanabe@gmail.com>
2015-12-09 15:13:10 -0200

    plot tools

---
+++ measuredPlot.m
@@ -0,0 +1,71 @@
+% Builds a set of subplots with the specified measures.
+%
+% written by: Renato Naville Watanabe 
+%
+%
+% ha = measuredPlot(numberOfRows, numberOfCols, unit, height, width, gapVertical, gapHorizontal, marginTop, ...
+%   marginBottom, marginLeft, marginRight)
+%   
+%
+% Inputs:
+%
+%   numberOfRows is the number of subplot rows that the figure will have.
+%
+%   numberOfCols is the number of subplot columns that the figure will have.
+%
+%   unit: string, length unit to be used. Possible units are 'pixels', 'normalized', 'inches', 
+%   'centimeters', 'points' and 'characters'.
+%
+%   figureHeight: float, height of each plot, in the established unit.
+%
+%   figureWidth: float, width of each plot, in the established unit.
+%
+%   gapVertical: float, vertical gap between the plots, in the established unit.
+%
+%   gapHorizontal: float, horizontal gap between the plots, in the established unit.
+%
+%   marginTop: float, margin in the top of the figure, in the established unit.
+%
+%   marginBottom: float, margin in the bottom of the figure, in the established unit.
+%
+%   marginLeft: float, margin in the left side of the figure, in the established unit.
+%
+%   marginRight: float, margin in the right side of the figure, in the established unit.
+
+
+
+function ha = measuredPlot(numberOfRows, numberOfCols, unit, height, width, gapVertical, gapHorizontal, marginTop, ...
+    marginBottom, marginLeft, marginRight)
+
+    % Calculation of axis width and height
+    axesWidth = (width - (marginLeft + marginRight) - (numberOfCols - 1) * gapHorizontal) / numberOfCols;
+    axesHeight = (height - (marginTop + marginBottom) - (numberOfRows - 1) * gapVertical) / numberOfRows;
+    
+    % Obtain screed dimensions to place figure in the centre
+    
+    set(0, 'units', unit);
+    screensize = get(0, 'screensize');
+    figureSize = [ screensize(3)/2 - width/2  screensize(4)/2 - height/2 width height];
+    
+    % Set bounding box equal to figure dimensions
+    set(gcf, 'units', unit,'PaperPosition', figureSize, 'Position',...
+        figureSize, 'Resize', 'off')
+    
+    % set each axes position
+    ha = zeros(numberOfRows * numberOfCols, 1);
+    
+    py = height - marginTop - axesHeight;
+    
+    figNumber = 1;
+    for i = 1:numberOfRows
+        px = marginLeft(1);
+            for j = 1:numberOfCols                
+                ha(figNumber) = axes('Units', unit, 'Position',[px py axesWidth axesHeight], ...
+                    'XtickLabel', '', 'YtickLabel', '');
+                px = px + axesWidth + gapHorizontal;
+                figNumber = figNumber + 1;
+            end
+        py = py - axesHeight - gapVertical;
+    end   
+
+end
