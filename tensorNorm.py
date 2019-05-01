0209fc1a95f866e46a9b4e631b5363960eed90a8
Renato Naville Watanabe <renato.navillewatanabe@gmail.com>
2019-05-01 10:52:36 -0300

    Create tensorNorm.py

---
+++ tensorNorm.py
@@ -0,0 +1,21 @@
+def tensorNorm(x, T):
+    '''
+    norm = tensorNorm(x, T)
+    
+    Performs a dot product for non-orthogonal vector spaces.
+    
+    Inputs:
+        -- x: the vector to have the norm computed.
+            
+        -- T: the tensor matrix of the vector space.
+
+    Output:
+        
+        -- norm: the norm of the vector x.
+    
+    '''
+    norm  = 0
+    for i in range(len(x)):
+        for j in range(i,len(x)): 
+            norm = norm + x[i]*x[j]*T[i,j]
+    return norm
